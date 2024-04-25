38f504ceffdb8fbda65f11f95ebe78bef8068fab
saanm23 <167672963+saanm23@users.noreply.github.com>
2024-04-25 14:41:55 +0000

    Neetedllop.java

--- 01_basics/loops/Nestedloop.java
+++ 01_basics/loops/Nestedloop.java
@@ -1,13 +1,27 @@
 package loops;
 
+// public class Nestedloop {
+
+//     public static void main(String[] args) {
+//         for(int i =1; i<=4; i++) {
+//             for(int j=1 ; j<=5; j++) {
+//                 System.out.print("*");
+//             }
+//             System.out.println(); // this is for next line only
+//         }
+//     }
+// }
+
 public class Nestedloop {
 
     public static void main(String[] args) {
-        for(int i =1; i<=4; i++) {
-            for(int j=1 ; j<=5; j++) {
+        int n = 4;
+        int m =5;
+        for(int i =1; i<=n; i++) {
+            for(int j=1 ; j<=m; j++) {
                 System.out.print("*");
             }
-            System.out.println("*");
+            System.out.println();
         }
     }
 }
