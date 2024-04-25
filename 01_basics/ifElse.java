a4c394e92b286e762fba85541db7e1709db321dd
saanm23 <167672963+saanm23@users.noreply.github.com>
2024-04-25 12:01:02 +0000

    ifElse.java

---
+++ 01_basics/ifElse.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+// public class ifElse {
+//     public static void main (String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int age = sc.nextInt();
+//         if (age > 18 ) {
+//             System.out.println("Adult");
+//         } else {
+//             System.out.println("Not Adult");
+//         }
+//     }
+// }
+
+public class ifElse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        if (age > 18) {
+            System.out.println("Adult"); 
+        } else {
+            System.out.println("Not Adult");
+        }
+    }
+
+    
+}
+
+
+
