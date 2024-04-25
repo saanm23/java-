fc608baeddfeed0ff8131b12c954ebcbcc2a558b
saanm23 <167672963+saanm23@users.noreply.github.com>
2024-04-25 12:11:36 +0000

    elseIf.java

---
+++ 01_basics/elseIf.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+//public class Elseif {
+//    public static void main(String args[]) {
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//
+//        if ( a == b) {
+//            System.out.println("equal");
+//        } else {
+//            if ( a > b) {
+//                System.out.println("a is greater");
+//            } else {
+//                System.out.println("a is lesser");
+//            }
+//        }
+//    }
+//}
+
+// public class elseIf { 
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+        
+//         if ( a == b) {
+//             System.out.println("equal");
+//         } else if (a > b) {
+//             System.out.println("a is greater");
+//         } else {
+//             System.out.println("a is lesser");
+//         }
+//     }
+// }
+
+public class elseIf {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a==b) {
+            System.out.println("Equal"); 
+        } else if (a > b) {
+                System.out.println(" a is greater");
+            } else {
+                System.out.println("a is lesser");
+            }
+        }
+    }
+
+    
