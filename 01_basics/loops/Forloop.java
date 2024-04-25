59825b6782270275c355a9a0f0aedb2bafc6b4fb
saanm23 <167672963+saanm23@users.noreply.github.com>
2024-04-25 13:36:28 +0000

    Forloop.java

---
+++ 01_basics/loops/Forloop.java
@@ -0,0 +1,25 @@
+package loops;
+
+// public class LoopFor {
+//     public static void main(String[] args) {
+//         // counter++ == counter = counter + 1
+        
+//         // for(int counter = 0 ; counter < 300 ; counter++) {
+
+//         //     System.out.println("Hello world");
+//         // }
+
+//         for(int i = 0 ; i < 11; i++) {
+//         System.out.println(i);
+//         }
+//     }
+// }
+
+public class Forloop {
+    public static void main(String[] args) {
+        for(int i=0; i<11; i++) {
+            System.out.println(i);
+        }
+    }
+   
+}
---
+++ 01_basics/loops/While.java
@@ -0,0 +1,26 @@
+package loops;
+// public class While {
+
+//         public static void main(String[] args) {
+    
+//              int i = 0;
+    
+//              while (i<11) {
+//                  System.out.println(i);
+//                  i++;
+//              }
+    
+//         }
+//     }
+    
+    
+public class While {
+
+    public static void main(String[] args) {
+        int i=0;
+        while (i<11) { 
+            System.out.println(i);   
+            i++;
+        } 
+    }
+}
