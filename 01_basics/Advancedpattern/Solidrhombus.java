aec97bcdccd64cb3934cb208a2d5240b64ee7aba
saanm23 <167672963+saanm23@users.noreply.github.com>
2024-04-27 08:19:51 +0000

    Numberpyramid.java

---
+++ 01_basics/Advancedpattern/Solidrhombus.java
@@ -0,0 +1,19 @@
+package loops;
+
+public class Solidrhombus {
+
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1; i<=5; i++) {
+            //spaces
+            for(int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+                //star
+                for(int j=1; j<=n; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
